ec8a6a9aa9ae9776c9fb6962939fda2054c5aa0f
Fariba <fariba.mohammadi1981@gmail.com>
2022-03-04 18:45:03 +0300

    init

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <footer >
+                Developed by @Fariba
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <h1>MY TO DO</h1>
+        </header>
+    )
+}
+
+export default Header;
